ad7a705b93341df2f45389c385891becb604b98a
fatouuuu <fatmaabdullrahim@gmail.com>
2022-11-15 11:23:12 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-function addingEventListener() {
+
+const input = document.getElementById('button');
+function addingEventListener(){
+    input.addEventListener('click', () => {
+        alert('I was clicked!');
+    });
 }
+
+addingEventListener();
