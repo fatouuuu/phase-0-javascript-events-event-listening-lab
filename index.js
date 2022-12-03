82324b0e95cfce8627183b2c70d3c57a75568ebc
fatouuuu <fatmaabdullrahim@gmail.com>
2022-12-03 11:01:18 +0300

    re-do for phase-1

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-
 const input = document.getElementById('button');
 function addingEventListener(){
     input.addEventListener('click', () => {
         alert('I was clicked!');
-    });
+    })
 }
 
-addingEventListener();
+addingEventListener()
